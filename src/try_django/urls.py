63a5e2e65376fd5b250db1cb420685b5b9b6dfbe
Fairfaxx <fede.lemaire@gmail.com>
2025-06-24 11:37:29 -0300

    rendering data from admin

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import (home_page, about_page, contact_page)
+from blog.views import (blog_post_detail_page)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='Contact Us'),
+    path('blog/', blog_post_detail_page),
 ]
