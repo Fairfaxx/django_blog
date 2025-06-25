0ac038a136a821efce25f30467d0944de04d9081
Fede <53632624+Fairfaxx@users.noreply.github.com>
2025-06-25 11:26:57 -0300

    Feature/main page for all blogs (#4)

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,8 +5,36 @@ from django.http import Http404
 
 
 
-def blog_post_detail_page(request, slug):
+def blog_post_list_view(request):
+  # List out objects
+  # Couldbe search
+  qs= BlogPost.objects.all()
+  template_name = 'blog_post_list.html'
+  context = {"object_list": qs}
+  return render(request, template_name, context)
+
+def blog_post_create_view(request):
+  # Create objects
+  # Using a form
+  template_name = 'blog_post_create.html'
+  context = {"form": None}
+  return render(request, template_name, context)
+
+def blog_post_detail_view(request, slug):
+  # 1 object or detail view
   obj = get_object_or_404(BlogPost, slug=slug)
   template_name = 'blog_post_detail.html'
+  context = {"object": obj}
+  return render(request, template_name, context)
+
+def blog_post_update_view(request):
+  obj = get_object_or_404(BlogPost, slug=slug)
+  template_name = 'blog_post_update.html'
+  context = {"object": obj, "form": None}
+  return render(request, template_name, context)
+
+def blog_post_delete_view(request):
+  obj = get_object_or_404(BlogPost, slug=slug)
+  template_name = 'blog_post_delete.html'
   context = {"object": obj}
   return render(request, template_name, context)
--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import (home_page, about_page, contact_page)
-from blog.views import (blog_post_detail_page)
+from blog.views import (blog_post_create_view)
 
 
 urlpatterns = [
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='Contact Us'),
-    path('blog/<str:slug>/', include ('blog.urls')),
+    path('blog-new/', blog_post_create_view),
+    path('blog/', include('blog.urls')),
 ]
