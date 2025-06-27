0d34769ba0eb19762ebd8facc268ac0bd1a21ef6
Fairfaxx <fede.lemaire@gmail.com>
2025-06-27 11:22:08 -0300

    Delete blog post and user login

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -12,9 +12,11 @@ class BlogPostModelForm(forms.ModelForm):
         model = BlogPost
         fields = ['title', 'slug', 'content']
 
-    def clean_title(self, *args, **kwargs):
+    def clean_title(self):
         title = self.cleaned_data.get('title')
         qs = BlogPost.objects.filter(title=title)
+        if self.instance is not None:
+            qs = qs.exclude(pk=self.instance.pk)
         if qs.exists():
-          raise forms.ValidationError("This title has already been used.")
+            raise forms.ValidationError("This title has already been used.")
         return title
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,18 +1,14 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-# path('', views.blog_post_detail_page)
-# ]
-
 from django.urls import path
-
-from .views import (blog_post_detail_view, blog_post_list_view,blog_post_update_view, blog_post_delete_view)
-
+from .views import (
+    blog_post_detail_view, 
+    blog_post_list_view,
+    blog_post_update_view, 
+    blog_post_delete_view
+)
 
 urlpatterns = [
-    path('', blog_post_list_view),
-    path('<str:slug>/', blog_post_detail_view),
-    path('/<str:slug>/edit', blog_post_update_view),
-    path('/<str:slug>/delete', blog_post_delete_view),
+    path('', blog_post_list_view, name='blog-list'),
+    path('<str:slug>/', blog_post_detail_view, name='blog-detail'),
+    path('<str:slug>/edit/', blog_post_update_view, name='blog-update'),
+    path('<str:slug>/delete/', blog_post_delete_view, name='blog-delete'),
 ]
